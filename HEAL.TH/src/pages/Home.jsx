083bf6e10e956b92028b508feee84333255097c5
Shityhanta <pauljohnanicete506@gmail.com>
2024-05-04 18:44:14 +0800

    afsfa

--- HEAL.TH/src/pages/Home.jsx
+++ HEAL.TH/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import greenCity from '../assets/greencity.png';
 import pmsh from '../assets/pmsh.jpg';
 import rosario from '../assets/rosario.jpg';
 import diosdado from '../assets/diosdado.jpg';
+import { Carousel, IconButton } from "@material-tailwind/react";
 
 const Home = () => {
   return (
@@ -27,25 +28,64 @@ const Home = () => {
           <h2 className='text-xl'>HEALTH CONDITIONS</h2>
           <CardsHome/>
         </div>    
-            
-        <div className='hospitals'>
-          <div className='hospitals-container'>
-            <div className='hospital-item' id='green-city'>
-              <img src={greenCity} style={{ zIndex: 4 }}></img>
-            </div>
-            <div className='hospital-item' id='pmsh'>
-              <img src={pmsh} style={{ zIndex: 3 }}></img>
-            </div>
-            <div className='hospital-item' id='rosario'>
-              <img src={rosario } style={{ zIndex: 2 }}></img>
-            </div>
-            <div className='hospital-item' id='diosdado'>
-              <img src={diosdado} style={{ zIndex: 1 }}></img>
-            </div>
-          </div>
-        </div>
       </section>
       
+      <Carousel
+        className="rounded-xl"
+        prevArrow={({ handlePrev }) => (
+          <IconButton
+            variant="text"
+            color="white"
+            size="lg"
+            onClick={handlePrev}
+            className="!absolute top-2/4 left-4 -translate-y-2/4"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+              />
+            </svg>
+          </IconButton>
+        )}
+        nextArrow={({ handleNext }) => (
+          <IconButton
+            variant="text"
+            color="white"
+            size="lg"
+            onClick={handleNext}
+            className="!absolute top-2/4 !right-4 -translate-y-2/4"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </IconButton>
+        )}
+      >
+        <img src={greenCity} alt="image 1" className="h-full w-full object-cover" />
+        <img src={pmsh} alt="image 2" className="h-full w-full object-cover" />
+        <img src={rosario} alt="image 3" className="h-full w-full object-cover" />
+        <img src={diosdado} alt="image 4" className="h-full w-full object-cover" />
+      </Carousel>
     </div>
   );
 }
