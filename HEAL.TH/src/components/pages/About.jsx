5882fc1cbf34bd74df79bf55ac4ec3d546c1b66b
Dune <platinumdunes613@gmail.com>
2024-05-09 11:35:55 +0800

    g

--- HEAL.TH/src/components/pages/About.jsx
+++ HEAL.TH/src/components/pages/About.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <h1>about</h1>
+      <h1>About Us</h1>
+      <section className='mission'>
+        <h1>MISSION</h1>
+      </section>
+
+      <section className='vision'>
+
+      </section>
     </div>
   )
 }
